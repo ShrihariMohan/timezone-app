c9158ff34c3a77390f5d33abb133a2b740cbd190
Shrihari <shrihari.uma.1999@gmail.com>
2026-01-09 18:18:34 +0530

    Fix - Remove cache on the timezone

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Toaster } from "@/components/ui/sonner"
-import RegisterSW from './register-sw'
 
 export const metadata: Metadata = {
   title: 'Timezone App',
@@ -44,7 +43,6 @@ export default function RootLayout({
       <body>
         {children}
         <Toaster />
-        <RegisterSW />
       </body>
       
     </html>
